261bbec56d6d57c8b664099f0e19f28d51985692
Ana Ferrandiz <anaferrandizpascual@gmail.com>
2024-07-10 09:21:22 +0200

    flors add Js

---
+++ js/flors.js
@@ -0,0 +1,34 @@
+// ************* CONTENIDO FLORS
+
+const selecText = document.querySelectorAll('.wrap__cadaflor')
+// seleccionamos texto
+const imagenes = document.querySelectorAll('.imgs__flors')
+// seleccionamos imágenes
+
+let posicion = 0
+
+let mostrarImagen = function () {
+    imagenes.forEach(function (each, i) {
+        imagenes[i].classList.remove('isVisible')
+        selecText[i].classList.remove('isActive')
+    })
+    imagenes[posicion].classList.add('isVisible')
+    selecText[posicion].classList.add('isActive')
+}
+
+selecText.forEach(function (each, i) {
+    selecText[i].addEventListener('mouseenter', function () {
+        posicion = i
+
+        mostrarImagen()
+    })
+    selecText[i].addEventListener('mouseleave', function () {
+        imagenes[i].classList.remove('isVisible')
+        selecText[i].classList.remove('isActive')
+
+    })
+
+
+})
+
+// ************* CONTENIDO FLORS
