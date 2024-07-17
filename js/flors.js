ede95637eca42f302b0e4ee7960cb8f2e6ac5212
Ana Ferrandiz <anaferrandizpascual@gmail.com>
2024-07-17 17:52:53 +0200

    Update flors.js

--- js/flors.js
+++ js/flors.js
@@ -32,3 +32,13 @@ selecText.forEach(function (each, i) {
 })
 
 // ************* CONTENIDO FLORS
+
+
+// ************* MENÚ RESPONSIVE
+const headerBtn = document.querySelector('.header__btn')
+const headerNav = document.querySelector('.header__nav')
+
+headerBtn.addEventListener('click', function(){
+    headerNav.classList.toggle('isActive')
+   })
+// ************* FIN MENÚ RESPONSIVE
